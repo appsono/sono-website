20069a06415cd6d670026a0ce8cc2a59d231e8e0
mathiiiiiis <mathis@mathiiis.de>
2026-09-25 19:42:19 +0200

    feat(icons): add icon set and sono-icon component

---
+++ src/components/SonoIcon.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from "vue";
+
+const icons = import.meta.glob("../assets/icons/*.svg", {
+  query: "?raw",
+  import: "default",
+  eager: true,
+});
+
+const props = defineProps({
+  name: { type: String, required: true },
+});
+
+const svg = computed(() => icons[`../assets/icons/${props.name}.svg`]);
+</script>
+
+<template>
+  <span class="sono-icon" aria-hiddeb="true" v-html="svg" />
+</template>
+
+<style lang="scss" scoped>
+.sono-icon {
+  display: inline-flex;
+  flex: none;
+  width: 1rem;
+  height: 1rem;
+
+  :deep(svg) {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
